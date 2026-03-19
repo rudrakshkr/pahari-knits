41e8e7ea56870f235c3ce1ca1a7a2bb9f0d04506
Rudraksh Kumar <rudrakshkumar2908@gmail.com>
2026-03-19 21:59:13 +0530

    Fix nodemailer

--- server/index.js
+++ server/index.js
@@ -87,7 +87,8 @@ if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
   mailer = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
-    secure: true,
+    secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
